9ae74bfa4d5c56ef74f42feda0a89f8ab6a8ab55
Glitch (puppeteer-example) <none>
2023-08-22 12:32:34 +0000

    🗽💃 Checkpoint ./server.js:60828816/1066

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express'),
     app = express(),
     puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
 
 app.get("/", async (request, response) => {
   try {
@@ -8,7 +11,23 @@ app.get("/", async (request, response) => {
       args: ['--no-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://developers.google.com/web/tools/puppeteer/');
+    await page._client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: path.resolve(__dirname, 'downloads') // Specify the desired download folder path
+  });
+    await page.goto('https://editor.fusionbrain.ai');
+      await page.waitForSelector('button._5VvP5');
+      await page.click('button._5VvP5');
+    await page.waitForSelector('div._78Teq');
+    await page.click('div._78Teq');
+    const textareaSelector = 'textarea';
+  const textToFill = 'cute cat';
+  await page.type(textareaSelector, textToFill);
+    await page.waitForSelector('button.I9yBt');
+    await page.click('button.I9yBt');
+    
+    
+    await page.waitForEvent('download');
     response.type('png').send(await page.screenshot());
     await browser.close();
   } catch (error) {
